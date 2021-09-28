cc20b8ed56b7f4247ed54c73ef04e22ad88088ab
Sai Varun <karnati187@gmail.com>
2021-09-28 21:43:33 +0530

    operators in js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 var arr=[1,2,3,4,"nmbr"]
 arr.push(4)
 console.log(arr.length)
+// arithmatic
+var a=15
+var b=10
+console.log('a+b=',a + b)
+console.log('a-b=',a - b)
+console.log('a*b=',a * b)
+//incr & decr
+var c = 5
+var d = 6
+console.log(c < d)
+console.log(c > d)
+
+console.log('c++=',c++)
+console.log('++c=',++c)
+
+var e=5
+var f=5
+console.warn(d == f)
+console.warn(d === f)
+
+// logical 
+console.log('T && T', true && true)
+console.log('T && F', true && false)
+console.log('F && T', false && true)
+console.log('F && F', false && false)
+
+console.log('T || T', true || true)
+console.log('T || F', true || false)
+console.log('F || T', false || true)
+console.log('F || F', false || false)
