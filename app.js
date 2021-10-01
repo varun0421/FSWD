a38f3383d4956277cf8cfd66d2214dc19318beed
Sai Varun <karnati187@gmail.com>
2021-10-01 09:50:11 +0530

    for loop in JS

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-var arr=[1,2,3,4,"nmbr"]
-arr.push(4)
-console.log(arr.length)
-// arithmatic
-var a=15
-var b=10
-console.log('a+b=',a + b)
-console.log('a-b=',a - b)
-console.log('a*b=',a * b)
-//incr & decr
-var c = 5
-var d = 6
-console.log(c < d)
-console.log(c > d)
+var list = document.querySelector('ul')
+console.log(list)
+var list1 = document.querySelector('ol')
+console.log(list1)
 
-console.log('c++=',c++)
-console.log('++c=',++c)
-
-var e=5
-var f=5
-console.warn(d == f)
-console.warn(d === f)
-
-// logical 
-console.log('T && T', true && true)
-console.log('T && F', true && false)
-console.log('F && T', false && true)
-console.log('F && F', false && false)
-
-console.log('T || T', true || true)
-console.log('T || F', true || false)
-console.log('F || T', false || true)
-console.log('F || F', false || false)
-
-g = 5
-h = 6
-console.log('g & h', g & h)
-console.log('g | h', g | h)
+var arr = ['Go to The GYM','cook','Eat']
+for(i = 0;i<arr.length;i++){
+    console.log(arr[i])
+}
