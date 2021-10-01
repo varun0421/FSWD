576ce87542d987244a22cc2dc5335c90274af5d6
Sai Varun <karnati187@gmail.com>
2021-10-01 12:31:29 +0530

    functions in JS

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var list = document.querySelector('ul')
+//function greet(Name){
+    //console.log(Name)
+    //console.log('Hello')
+//}
+//greet('Anisha,19')
 
-
-var arr = ['GYM', 'cook','EAT']
-arr.push('assg')
-for (var i = 0; i < arr.length ;i++) {
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-    element.appendChild(textNode)
-    list.appendChild(element)
+function add(a=0,b=0){
+    sum = a + b
+    return sum
 }
+
+var add = (a,b) =>a + b
+
+
+console.log(add(10,20))
