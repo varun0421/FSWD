be2a7b47fb624eb73b12f3d2db86d194fb29bc98
Sai Varun <karnati187@gmail.com>
2021-10-01 19:14:25 +0530

    calculator in JS

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-var button = document.querySelector('button')
-var buody = document.querySelector('body')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const sub = document.querySelector('#sub')
+const resultBox = document.querySelector('.result')
 
-
-const callbackfunc = (event) => {
-    buody.classList.toggle('dark')    
+const sum = () => {
+    const result = parseInt(a.value) + parseInt(b.value)
+    resultBox.innerHTML = result
 }
-button.addEventListener ('click', callbackfunc)
+const subract = () => {
+    const result = parseInt(a.value) - parseInt(b.value)
+    resultBox.innerHTML = result
+
+const product = () => {
+    const result = parseInt(a.value) * parseInt(b.value)
+    resultBox.innerHTML = result
+}
+
+
+const calculate = (event, operation) => {
+    // console.log(event.target)
+    // if (operation == 'add') {
+    //     console.log('adding')
+    // } else if (operation == 'multiply') {
+    //     console.log('multiply')
+    // }
+}
+
+switch (operation) {
+    case "add":
+        resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+        break
+    case "sub":
+        resultBox.innerHTML = parseInt(a.value) - parseInt(b.value) 
+        break
+    case "multiply":
+        resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)    
+        break
+    default:
+        resultBox.innerHTML = "Not a Valid expresssion"    
+}
+
+add.addEventListener('click', (event) => {
+    calculate(event, 'add')
+})
+
+
+multiply.addEventListener('click', (event) => {
+    calculate(event, 'multiply')
+})
