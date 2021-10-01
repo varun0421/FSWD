d3ddf4d0eebcf7841e4c0cba02879291ed89fa49
Sai Varun <karnati187@gmail.com>
2021-10-01 11:03:24 +0530

    appending elements to list using jS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var list = document.querySelector('ul')
 
+
 var arr = ['GYM', 'cook','EAT']
+arr.push('assg')
 for (var i = 0; i < arr.length ;i++) {
     var element = document.createElement('li')
     var textNode = document.createTextNode(arr[i])
