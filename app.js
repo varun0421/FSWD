d28d394c526a18e05eeb72fa1ca89ef3f8f6322b
Sai Varun <karnati187@gmail.com>
2021-10-01 19:31:55 +0530

    array element operations

--- app.js
+++ app.js
@@ -1,52 +1,16 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const add = document.querySelector('#add')
-const multiply = document.querySelector('#multiply')
-const sub = document.querySelector('#sub')
-const resultBox = document.querySelector('.result')
+var arr = [0,1,2,3,4,5]
 
-const sum = () => {
-    const result = parseInt(a.value) + parseInt(b.value)
-    resultBox.innerHTML = result
-}
-const subract = () => {
-    const result = parseInt(a.value) - parseInt(b.value)
-    resultBox.innerHTML = result
+y= arr.splice(2,1)
 
-const product = () => {
-    const result = parseInt(a.value) * parseInt(b.value)
-    resultBox.innerHTML = result
-}
+// y = []
+// y= arr.filter((Element,index) => {
+//     if (Element > 3)
+//     return true
+// })
+// for (var i =0; i < arr.length; i++){
+//     if (arr[i] % 2 == 0) 
+//     y.push(arr[i])
+// }
 
-
-const calculate = (event, operation) => {
-    // console.log(event.target)
-    // if (operation == 'add') {
-    //     console.log('adding')
-    // } else if (operation == 'multiply') {
-    //     console.log('multiply')
-    // }
-}
-
-switch (operation) {
-    case "add":
-        resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
-        break
-    case "sub":
-        resultBox.innerHTML = parseInt(a.value) - parseInt(b.value) 
-        break
-    case "multiply":
-        resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)    
-        break
-    default:
-        resultBox.innerHTML = "Not a Valid expresssion"    
-}
-
-add.addEventListener('click', (event) => {
-    calculate(event, 'add')
-})
-
-
-multiply.addEventListener('click', (event) => {
-    calculate(event, 'multiply')
-})
+console.log(y)
+console.log(arr)
