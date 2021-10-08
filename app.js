1c43a8bed7e2c5886560ff932be74e80b7a580f1
Sai Varun <karnati187@gmail.com>
2021-10-08 10:12:46 +0530

    calculator

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-var arr = [0,1,2,3,4,5]
-
-y= arr.splice(2,1)
-
-// y = []
-// y= arr.filter((Element,index) => {
-//     if (Element > 3)
-//     return true
-// })
-// for (var i =0; i < arr.length; i++){
-//     if (arr[i] % 2 == 0) 
-//     y.push(arr[i])
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const sub = document.querySelector('#subract')
+const multiply = document.querySelector('#multiply')
+const resultBox  = document.querySelector('.result')
+ 
+// const sum = ()=>{
+//     const result = parseInt(a.value) + parseInt(b.value)
+//     resultBox.innerHTML = result
 // }
 
-console.log(y)
-console.log(arr)
+// const mul = ()=>{
+//     const result = parseInt(a.value) * parseInt(b.value)
+//     resultBox.innerHTML = result
+// }
+const calculate = (event,operation) => {
+    // if (operation == 'add'){
+    //     const result = parseInt(a.value) + parseInt(b.value)
+    //     console.log(result)
+    //     resultBox.innerHTML = result  
+    // }
+    // else if (operation == 'multiply'){
+    //     const result = parseInt(a.value) * parseInt(b.value)
+    //     console.log(result)
+    //     resultBox.innerHTML = result    
+    // }
+    switch(operation){
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "multiply":
+            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)   
+            break
+        case "subract":
+            resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)    
+            break
+        default :
+        resultBox.innerHTML = "Not Valid operation"    
+    }
+    
+    
+}
+// add.addEventListener('click',sum)
+// multiply.addEventListener('click',mul)
+
+add.addEventListener('click', () => calculate(event,'add'))
+sub.addEventListener('click', () => calculate(event,'subract'))
+multiply.addEventListener('click', () => calculate(event,'multiply'))
